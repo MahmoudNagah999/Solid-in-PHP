f40222ca29a4957130665461d17415ebafd5fe3f
MahmoudNagah999 <Mahmoudnagah999@gmail.com>
2023-12-22 10:20:54 +0200

    wrong single responsipilty

---
+++ Classes/matchClass.php
@@ -0,0 +1,14 @@
+<?php
+class matchClass
+{
+    public function attack(){
+        return "attack";
+    }
+    public function defence(){
+        return "defence";
+    }
+    public function keeper(){
+        return "keeper";
+    }
+
+}
