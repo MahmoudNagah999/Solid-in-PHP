6140d7b4c7f191a4cf15b33d6577d0eb3d1fc5e2
MahmoudNagah999 <Mahmoudnagah999@gmail.com>
2024-07-20 21:46:45 +0300

    save

---
+++ Classes/Match.php
@@ -0,0 +1,16 @@
+<?php
+
+class FootBallMatch 
+{
+    public function attack(){
+        return "attack";
+    }
+
+    public function defend(){
+        return "defend";
+    }
+
+    public function keeper(){
+        return "keeper";
+    }
+}
